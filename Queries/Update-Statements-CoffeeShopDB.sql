788cec4bcc2397e6599113b60f8772082d4bbb9b
Azhaar <124681173+Azhaar01@users.noreply.github.com>
2025-07-06 15:37:31 +0300

    Add files via upload

---
+++ Queries/Update-Statements-CoffeeShopDB.sql
@@ -0,0 +1,11 @@
+-- Set salary to 6000 for the employee whose first name is 'Hassen'
+UPDATE Employees
+SET Salary = 6000
+WHERE FirstName = 'Hassen'
+SELECT * FROM Employees
+
+-- Update the bill date of bill number 200 to the current date:
+UPDATE Bills 
+SET Bill_Date = GETDATE()
+WHERE Bill_Number = 24878
+SELECT * FROM Bills
